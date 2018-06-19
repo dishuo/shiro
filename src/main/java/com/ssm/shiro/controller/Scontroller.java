5ebf31f59914e2802359d7b5b218f3bde282edb9
wangshanghello <35395563+wangshanghello@users.noreply.github.com>
2018-06-19 09:22:24 +0800

    这是一个ssm+shiro的登录例子

---
+++ src/main/java/com/ssm/shiro/controller/Scontroller.java
@@ -0,0 +1,47 @@
+package com.ssm.shiro.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.crypto.hash.Md5Hash;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ssm.shiro.entity.Entity;
+
+@Controller
+@RequestMapping("/user") 
+public class Scontroller {
+	
+	  //用户登录  
+    @RequestMapping("/login")  
+    public String login(Entity user, HttpServletRequest request){  
+    	System.out.println("user:"+user.getUsername());
+        Subject subject=SecurityUtils.getSubject();  
+        ByteSource salt =  ByteSource.Util.bytes(user.getUsername());
+		//加密
+		String getpassword = new Md5Hash(user.getPassword(), salt).toString();
+        UsernamePasswordToken token=new UsernamePasswordToken(user.getUsername(),getpassword);  
+        try {  
+            //调用subject.login(token)进行登录，会自动委托给securityManager,调用之前  
+            subject.login(token);//会跳到我们自定义的realm中  
+            request.getSession().setAttribute("user",user);  
+            return "success";  
+        } catch (AuthenticationException ae) {
+            //unexpected condition?  error?
+        	System.out.println("登录失败: " + ae.getMessage());
+        	return "redirect:/login.jsp";
+        }
+        
+    }  
+	
+	   @RequestMapping("/logout")  
+	    public String logout(HttpServletRequest request){  
+	        request.getSession().invalidate();  
+	        return "login";  
+	    } 
+}
